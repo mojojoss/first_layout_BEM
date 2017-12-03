c0211048b7872a8ee0a1162728d13d524a32dc32
Mojo <mojojoss1@gmail.com>
2017-12-03 20:43:13 +0700

    first_commit

---
+++ main/logo-main/logo-main.js
@@ -0,0 +1,1 @@
+document.getElementById('logo-main').innerHTML = '<div class="Elipse_1"><div class="Elipse_1_copia"></div></div><h2 class="name-top">NAME HERE</h2><h2 class="surname-top">SURNAME</h2><h3 class="designer-top">GRAPHIC DESIGNER</h3>'
